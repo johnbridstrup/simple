210c73989d421cf2b5be5ed7b6022bc0a5662c46
john <john.bridstrup@gmail.com>
2018-10-02 16:11:52 -0400

    Bump version: 0.1.3 → 0.1.4

--- simple/__init__.py
+++ simple/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """John Bridstrup"""
 __email__ = 'john.bridstrup@gmail.com'
-__version__ = '__version__ = '0.1.3''
+__version__ = '__version__ = '0.1.4''
 
 # import Data
 # import data_analysis
